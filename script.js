ba31fc3b6846b03b677881ab102f9f88d4f92861
Suji-12 <23csec23sujithas@gmail.com>
2025-11-26 20:18:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 function createTable() {
     //Write your code here
+	const rn = prompt("Input number of rows");
+    const cn = prompt("Input number of columns");
+    if (
+        isNaN(rn) || isNaN(cn) ||    
+        rn <= 0 || cn <= 0       
+    ) {
+        alert("Please enter valid positive numbers.");
+        return;
+    }
+
+    const table = document.getElementById("myTable");
+    table.innerHTML = "";
+    for (let i = 0; i < rn; i++) {
+        const row = document.createElement("tr");
+
+        for (let j = 0; j < cn; j++) {
+            const cell = document.createElement("td");
+            cell.textContent = `Row-${i} Column-${j}`;
+            row.appendChild(cell);
+        }
+
+        table.appendChild(row);
+    }
   
 }
